10466ced92d763f4db2aa392ab00d1e10aa3cc13
jawaharSakthi <jawaharsakthivenugopal07@gmail.com>
2023-02-25 00:16:35 +0530

    BGCOLOR

--- css/Spotifyaaa.js
+++ css/Spotifyaaa.js
@@ -9,3 +9,14 @@ greet='Good Afternoon!';
 else if(hrs>=17 && hrs<=24)
 greet='Good Evening!';
 document.getElementById('message').innerHTML='<b>'+greet+'</b>';
+
+let spa=document.getElementsByClassName("Music_box1")[0];
+let mainDiv=document.getElementsByClassName("main_div")[0];
+
+spa.addEventListener("mouseover",over)
+
+function over()
+{
+    
+}
+
