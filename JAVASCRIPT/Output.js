5339db7dc10284da949915cbb1ade9d2aa262b18
jawaharSakthi <jawaharsakthivenugopal07@gmail.com>
2023-03-10 10:17:30 +0530

    zee5

---
+++ JAVASCRIPT/Output.js
@@ -0,0 +1,19 @@
+let a="Students";
+document.write(a);
+document.writeln(a);
+document.writeln(a);
+document.write("Hello<br>"+"JAWA");
+
+
+// console.log output
+let v="JAWA";
+console.log(v);
+
+confirm("HELLO ALL")
+
+let x=Number(prompt("Enter the value of a"));
+let y=Number(prompt("Enter the value of b"));
+let z=(x+y);
+console.log(z);
+
+alert("BYEE ALL")
